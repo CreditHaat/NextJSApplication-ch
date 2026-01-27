cc37eb6c9e22997cd8fec385d3377e5d178d9416
CreditHaat <rahulgupta.credithaat@gm1il.com>
2026-01-27 11:16:54 +0530

    cpi yes chnages on embedded journey by tejas D

--- components/EmbeddedJourneyList/ServerSidePropsFunction.jsx
+++ components/EmbeddedJourneyList/ServerSidePropsFunction.jsx
@@ -5,11 +5,7 @@ import CHEmbeddedListCards from './CHEmbeddedListCards';
 import Loader from './Toader';
 import EmbeddedListNavbar from './EmbeddedListNavbar';
 import CompanyPerformance from './CompanyPerformance';
-// import EmblaCarousel from "../../components/NewBlJourneyD/Emblacarousel/js/EmblaCarousel";
 import EmblaCarousel from "../../components/EmbeddedJourneyList/Emblacarousel/js/EmblaCarousel";
-// import listimage1 from '../../components/NewBlJourneyD/newblimages/newblfirstpageimg11.png';
-// import listimage2 from '../../components/NewBlJourneyD/newblimages/newblfirstpageimg2.png';
-// import listimage3 from '../../components/NewBlJourneyD/newblimages/newblfirstpageimg33.png';
 import listimage1 from '../../components/NewBlJourneyD/newblimages/banner11.png';
 import listimage2 from '../../components/NewBlJourneyD/newblimages/banner22.png';
 import listimage3 from '../../components/NewBlJourneyD/newblimages/banner333.png';
@@ -26,12 +22,15 @@ const SLIDES = [
 
 
 
-const getData = async ({searchParams}) => {
+const getData = async (searchParams) => {
 
     const formData1 = new FormData();
     formData1.append('mobilenumber',searchParams.mobilenumber);
+    formData1.append('cpi',searchParams.cpi);
 
     try{
+      console.log('BASE URL:', process.env.NEXT_PUBLIC_REACT_APP_BASE_URL);
+
       const response =await axios.post(`${process.env.NEXT_PUBLIC_REACT_APP_BASE_URL}lenderslist1`, formData1, {
         headers: {
           'Content-Type': 'application/json',
@@ -44,120 +43,26 @@ const getData = async ({searchParams}) => {
     }
     
     return null;
-
-    // if(!response.ok){
-    //     throw Error;
-        
-    // }
-    // return response.json();
     
-}
-
-const getUrlLink = (searchParams) => {
-  return Object.keys(searchParams).length ? 
-    Object.entries(searchParams)
-      .map(([key, value]) => `${key}=${value}`)
-      .join('&')
-    : 'null';
-};
-
-// const OTPGenerate = async ({searchParams}) => {
-//   // e.preventDefault();
-
-//   // const localStatus = localStorage.getItem('verifiedOTP');
-//   // if(localStatus){
-//     // console.log("localStorage valule is :: ",localStatus);
-//   // }
-
-//   // const localStatus = false;
-
-//   // if (localStatus === "false") {
-//       try {
-//           // setIsVisible(true);
-//           const queryParams = Object.fromEntries(new URLSearchParams(searchParams));
-//           // const queryParams = new URLSearchParams(location.search);
-
-//           // Retrieve values for the specified parameters
-//           // const channel = queryParams.get('channel') || '';
-//           const channel = queryParams.channel;
-//           // const dsa = queryParams.get('dsa') || '';
-//           const dsa = queryParams.dsa;
-//           // const source = queryParams.get('source') || '';
-//           const source = queryParams.source;
-//           // const subSource = queryParams.get('sub_source') || '';
-//           const subSource = queryParams.sub_source;
-//           // const subDsa = queryParams.get('sub_dsa') || '';
-//           const subDsa = queryParams.sub_dsa;
-
-//           // console.log(mobileNumber);
-//           // console.log("Inside OTP Generate....................., mobileNumber : ", mobileNumber);
-
-//           // const urllink = location.search?.split('?')[1] || 'null';
-//           const urllink = getUrlLink(searchParams);
-
-//           const formData1 = new FormData();
-//           formData1.append('mobilenumber',searchParams.mobilenumber);
-//           // formData1.append('userPhoneNumber', mobileNumber);
-//           // formData1.append('firstName', formData.firstName);
-//           // formData1.append('lastName', formData.lastName);
-//           // formData1.append('profession', formData.profession);
-//           formData1.append('dsa', dsa);
-//           formData1.append('channel', channel);
-//           formData1.append('source', source);
-//           formData1.append('sub_source', subSource);
-//           formData1.append('campaign', urllink);
-//           formData1.append('sub_dsa', subDsa);
-
-
-//           // const response = await axios.post(`${process.env.REACT_APP_BASE_URL}chfronetendotpgenerator`, formData1, {
-//           //     headers: {
-//           //         'Content-Type': 'application/json',
-//           //     },
-//           // });
-
-//           console.log("Inside the OTPGenerate");
-
-//           const response = await axios.post(`${process.env.NEXT_PUBLIC_REACT_APP_BASE_URL}chEmbeddedList_OTPGenerate`, formData1);
-
-//           console.log("After the OTP Generate");
-
-//           if (response.data.code === 0) {
-
-//               setStgOneHitId(response.data.obj.stgOneHitId);
-//               setstgTwoHitId(response.data.obj.stgTwoHitId);
-//               sett_experian_log_id(response.data.obj.t_experian_log_id);
-
-//           }
-
-//           console.log("tejas", response);
-
-//           if (response.status === 200) {
-//               console.log('Submission successful:', response.data);
-//           } else {
-//               console.error('Submission failed:', response.statusText);
-//           }
-//       } catch (error) {
-//           console.error('Error submitting form:', error);
-//       }
-//   // } 
-//   // else {
-//       // console.log("Inside else");
-//       // getLoanBackend(lenderProduct);
-//   // }
-
-// };
+   }
 
 const ServerSidePropsFunction = async ({params, searchParams}) => {
-    const response = await getData({searchParams});
+    // const response = await getData({searchParams});
 
     // const response = await OTPGenerate({searchParams});
 
     // response=null;
 
     // const queryParams = Object.fromEntries(new URLSearchParams(searchParams));
-    const queryParams = searchParams ?? {};
+    // const queryParams = searchParams ?? {};
+    const queryParams = await searchParams; // ✅ THIS IS REQUIRED
+
     const header = queryParams.ch_header;
     const mobile = queryParams.mobilenumber;
+    const cpi = queryParams.cpi;
+
+    const response = await getData(queryParams);
+
 
   return (
     <div>
@@ -207,4 +112,3 @@ const ServerSidePropsFunction = async ({params, searchParams}) => {
 }
 
 export default ServerSidePropsFunction
-
